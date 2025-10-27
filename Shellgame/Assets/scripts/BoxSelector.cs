dd044418300ec0c43d11769f2d097672d3d519fe
PaolaChaparro80 <pmchaparrog@unadvirtual.edu.co>
2025-10-27 18:55:46 -0500

    Initial project setup
    created project folder, designed game environment, implemented core mechanics, and added some sound effects."

---
+++ Shellgame/Assets/scripts/BoxSelector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BoxSelector : MonoBehaviour
+{
+    public int index;
+    private ShellGameManager manager;
+
+    void Start()
+    {
+        manager = FindObjectOfType<ShellGameManager>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && manager != null)
+        {
+            manager.SelectBox(index);
+        }
+    }
+}
+
+
+
